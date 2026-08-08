106c3010fb6ea505915630c75230f6311fa5b145
Rohan <rohan.sharma@timesgroup.com>
2026-08-08 23:26:01 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-portfolio_003/",
+  base: process.env.VITE_BASE || "/my-portfolio_003/",
   plugins: [react()],
   build: {
     outDir: "build",
